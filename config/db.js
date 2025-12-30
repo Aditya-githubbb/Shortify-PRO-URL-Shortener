eac3fa1dee0ed5efe4311c0bae34df8a4083dfa3
Aditya-githubbb <vermaaditya448192@gmail.com>
2025-12-30 22:15:02 +0530

    fix: conditional SSL for MySQL (local + production)

--- config/db.js
+++ config/db.js
@@ -1,2 +1,16 @@
+import mysql from "mysql2/promise";
 import { drizzle } from "drizzle-orm/mysql2";
-export const db = drizzle(process.env.DATABASE_URL);
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+
+  ...(isProduction && {
+    ssl: {
+      rejectUnauthorized: true,
+    },
+  }),
+});
+
+export const db = drizzle(pool);
