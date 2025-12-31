e91d4433bcff3c087d803e22b3d0136a28e1c461
Aditya-githubbb <vermaaditya448192@gmail.com>
2025-12-31 13:05:36 +0530

    fix: db config for railway

--- config/db.js
+++ config/db.js
@@ -1,16 +1,6 @@
 import mysql from "mysql2/promise";
 import { drizzle } from "drizzle-orm/mysql2";
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
-
-  ...(isProduction && {
-    ssl: {
-      rejectUnauthorized: true,
-    },
-  }),
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 export const db = drizzle(pool);
